f982f15034ad89c812fdbd4c1e2c3250a5418020
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-03 16:57:56 -0700

    deleted Body.js and moved select and options back to App.js.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
-import Body from './Body.js'
 import ImgList from './image-list.js';
 import creatures from './horned-creatures.js';
 class App extends Component {
@@ -18,7 +17,20 @@ class App extends Component {
     return ( 
       <>
         <Header />
-        <Body />
+        <select onChange={this.showChanges}>
+            <option value="All">ALL THE MONSTERS</option>
+            <option value="narwhal">Narwhals Narwhals</option>
+            <option value="rhino">Rhino will RYNO</option>
+            <option value="unicorn">OMG! Unicorns!</option>
+            <option value="unilego">Unilego?? UM WAT?!</option>
+            <option value="triceratops">This triceratops and star leaves, I swear..</option>
+            <option value="markhor">Markhor</option>
+            <option value="mouflon">Mouflon making me crazy</option>
+            <option value="addax">Addaxing all the questions</option>
+            <option value="chameleon">Chameleon comes and goes</option>
+            <option value="lizard">Lizards, love them</option>
+            <option value="dragon">Smaug the dragon</option>
+          </select>
         <section>
         <ImgList 
           creatures={filterMonsters}
