455ded67d09d046b06f6ec6c6ac02da9508cffaf
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-03 15:08:17 -0700

    added options for each individual keywordof the horned-creatures.js data.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,16 @@ class App extends Component {
         <select onChange={this.showChanges}>
           <option value="All">ALL THE MONSTERS</option>
           <option value="narwhal">Narwhals Narwhals</option>
-          <option value=""
+          <option value="rhino">Rhino will RYNO</option>
+          <option value="unicorn">OMG! Unicorns!</option>
+          <option value="unilego">Unilego?? UM WAT?!</option>
+          <option value="triceratops">This triceratops and star leaves, I swear..</option>
+          <option value="markhor">Markhor</option>
+          <option value="mouflon">Mouflon making me crazy</option>
+          <option value="addax">Addaxing all the questions</option>
+          <option value="chameleon">Chameleon comes and goes</option>
+          <option value="lizard">Lizards, love them</option>
+          <option value="dragon">Smaug the dragon</option>
         </select>
       <ImgList 
         creatures={filterMonsters}
