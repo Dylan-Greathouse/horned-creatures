2f169efee395b43a536cd537a8b1f47f77777caf
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-04 14:54:12 -0700

    forgot to switch to a new branch until after i finished, chan I made changes.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import './App.css';
 import Header from './Header';
 import ImgList from './image-list.js';
 import creatures from './horned-creatures.js';
+import Dropdown from './Dropdown.js';
+
+
 class App extends Component {
   state = { 
       keyword: 'All',
+      horns: 'All'
    };
+   options = ['All', 'narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops',
+  'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon'];
+   hornsOptions = ['All', 1, 2, 3, 100];
+
+
    showChanges = (e) => {
      this.setState({ keyword: e.target.value });
+   };
+
+   showHornChange = (e) => {
+    const numHorns = e.target.value;
+    this.setState({ horns: numHorns });
    };
   render() { 
     const filterMonsters = creatures.filter(
       (creature) => this.state.keyword === 'All' || creature.keyword === this.state.keyword
     );
+    const filterMonstersHorns = filterMonsters.filter(
+      (creature) => this.state.horns === 'All' || creature.horns === Number(this.state.horns)
+    );
     return ( 
       <>
         <Header />
-        <select onChange={this.showChanges}>
+        
+        <Dropdown  options={this.options} changeEvent={this.showChanges} />
+        <Dropdown  options={this.hornsOptions} changeEvent={this.showHornChange} />
+        
+        {/* <select onChange={this.showChanges}>
             <option value="All">ALL THE MONSTERS</option>
             <option value="narwhal">Narwhals Narwhals</option>
             <option value="rhino">Rhino will RYNO</option>
@@ -30,10 +51,10 @@ class App extends Component {
             <option value="chameleon">Chameleon comes and goes</option>
             <option value="lizard">Lizards, love them</option>
             <option value="dragon">Smaug the dragon</option>
-          </select>
+          </select> */}
         <section>
         <ImgList 
-          creatures={filterMonsters}
+          creatures={filterMonstersHorns}
           />
           </section>
       </>
